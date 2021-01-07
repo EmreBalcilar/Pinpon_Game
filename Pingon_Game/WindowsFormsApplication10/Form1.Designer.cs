c1298c5a4d9b75a10414dc56f78a16b1fd92d985
EmreBalcilar <emrebalcilar@ogrenci.beykoz.edu.tr>
2021-01-07 19:12:15 +0300

    C# Basic_Pingon_Game
    Basic Pingon Game
    the background changes color and the score increases as the ball hits the bottom racket.

---
+++ Pingon_Game/WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.arkaplan = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.oyunbitti = new System.Windows.Forms.Label();
+            this.top = new System.Windows.Forms.PictureBox();
+            this.raket = new System.Windows.Forms.PictureBox();
+            this.topskor = new System.Windows.Forms.Label();
+            this.skorsay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.arkaplan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // arkaplan
+            // 
+            this.arkaplan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.arkaplan.Controls.Add(this.label1);
+            this.arkaplan.Controls.Add(this.oyunbitti);
+            this.arkaplan.Controls.Add(this.top);
+            this.arkaplan.Controls.Add(this.raket);
+            this.arkaplan.Controls.Add(this.topskor);
+            this.arkaplan.Controls.Add(this.skorsay);
+            this.arkaplan.Cursor = System.Windows.Forms.Cursors.No;
+            this.arkaplan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.arkaplan.Location = new System.Drawing.Point(0, 0);
+            this.arkaplan.Name = "arkaplan";
+            this.arkaplan.Size = new System.Drawing.Size(703, 423);
+            this.arkaplan.TabIndex = 0;
+            this.arkaplan.Paint += new System.Windows.Forms.PaintEventHandler(this.arkaplan_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(510, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 73);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // oyunbitti
+            // 
+            this.oyunbitti.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.oyunbitti.AutoSize = true;
+            this.oyunbitti.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.oyunbitti.Location = new System.Drawing.Point(91, 78);
+            this.oyunbitti.Name = "oyunbitti";
+            this.oyunbitti.Size = new System.Drawing.Size(446, 220);
+            this.oyunbitti.TabIndex = 4;
+            this.oyunbitti.Text = "Oyun Bitti\r\n\r\n-F1 Restart \r\n-ESC Oyundan Çık\r\n";
+            this.oyunbitti.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.oyunbitti.Click += new System.EventHandler(this.oyunbitti_Click);
+            // 
+            // top
+            // 
+            this.top.BackColor = System.Drawing.Color.Red;
+            this.top.Location = new System.Drawing.Point(587, 34);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(30, 30);
+            this.top.TabIndex = 1;
+            this.top.TabStop = false;
+            // 
+            // raket
+            // 
+            this.raket.BackColor = System.Drawing.Color.Black;
+            this.raket.Location = new System.Drawing.Point(190, 329);
+            this.raket.Name = "raket";
+            this.raket.Size = new System.Drawing.Size(200, 20);
+            this.raket.TabIndex = 0;
+            this.raket.TabStop = false;
+            // 
+            // topskor
+            // 
+            this.topskor.AutoSize = true;
+            this.topskor.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.topskor.Location = new System.Drawing.Point(3, 9);
+            this.topskor.Name = "topskor";
+            this.topskor.Size = new System.Drawing.Size(155, 55);
+            this.topskor.TabIndex = 2;
+            this.topskor.Text = "Skor :";
+            // 
+            // skorsay
+            // 
+            this.skorsay.AutoSize = true;
+            this.skorsay.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.skorsay.Location = new System.Drawing.Point(149, 9);
+            this.skorsay.Name = "skorsay";
+            this.skorsay.Size = new System.Drawing.Size(52, 55);
+            this.skorsay.TabIndex = 3;
+            this.skorsay.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 423);
+            this.Controls.Add(this.arkaplan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.arkaplan.ResumeLayout(false);
+            this.arkaplan.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.raket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel arkaplan;
+        private System.Windows.Forms.PictureBox top;
+        private System.Windows.Forms.PictureBox raket;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label skorsay;
+        private System.Windows.Forms.Label topskor;
+        private System.Windows.Forms.Label oyunbitti;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pingon_Game/WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        public int hız_sol = 4;
+        public int top_hız = 4;
+        public int puan = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Visible = false;
+            timer1.Enabled = true;
+            Cursor.Hide(); 
+            this.FormBorderStyle = FormBorderStyle.None; 
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            raket.Top = arkaplan.Bottom - (arkaplan.Bottom / 10);  
+            oyunbitti.Left = (arkaplan.Width / 2) - (oyunbitti.Width / 2);  
+            oyunbitti.Top = (arkaplan.Height / 2) - (oyunbitti.Height / 2);
+            oyunbitti.Visible = false;
+   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            raket.Left = Cursor.Position.X - (raket.Width / 2); 
+
+            top.Left += hız_sol ; 
+            top.Top += top_hız;
+            
+            if (top.Bottom >= raket.Top && top.Bottom <= raket.Bottom && top.Left >= raket.Left && top.Right <= raket.Right) //raket çarpışması
+            {
+                top_hız += 2;
+                hız_sol += 2;
+                top_hız = -top_hız; 
+                puan += 1; 
+                skorsay.Text = puan.ToString();
+                int x, y, z;
+                Random rnd = new Random(); 
+                x = rnd.Next(255);
+                y = rnd.Next(255);
+                z = rnd.Next(255);
+                arkaplan.BackColor = Color.FromArgb(x, y, z); 
+                SoundPlayer ses = new SoundPlayer();
+                string dizin = Application.StartupPath + "\\song.wav";  
+                ses.SoundLocation = dizin;
+                ses.Play();
+
+
+            }
+            if (top.Left <= arkaplan.Left)
+            {
+                hız_sol = -hız_sol;
+            }
+            if (top.Right >= arkaplan.Right)
+            {
+                hız_sol = -hız_sol;
+            }
+            if (top.Top <= arkaplan.Top)
+            {
+                top_hız = -top_hız;
+            }
+            if (top.Bottom >= arkaplan.Bottom)  
+            {
+                timer1.Enabled = false;  
+                oyunbitti.Visible = true;
+                label1.Visible = true;
+                if (puan <= 1)
+                {
+
+                    label1.Text = "Çok Kötüsün";
+                }
+                if (puan == 1)
+                {
+
+                    label1.Text = "Çok Kötüsün";
+                }
+                if (puan >= 4)
+                {
+
+                    label1.Text = "İdare Eder Bir Yeteneğin Var";
+                }
+                if (puan >= 7)
+                {
+
+                    label1.Text = "Hmm Fena Değil";
+                }
+                if (puan >= 9)
+                {
+                    label1.Text = "Cidden Bu Konuda Yeteneklisin";
+                }
+                if (puan >= 15)
+                {
+                    label1.Text = "Mükemmel";
+                }
+                if (puan >= 20)
+                {
+                    label1.Text = "Rekor Sende";
+                }
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) 
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.F1) 
+            {
+                top.Top = 100;
+                top.Left = 100;
+                hız_sol = 4;
+                top_hız = 4;
+                puan = 0;
+                skorsay.Text = "0";
+                timer1.Enabled = true;
+                oyunbitti.Visible = false;
+                arkaplan.BackColor = Color.White;
+                label1.Visible = false;
+            }
+        }
+        
+        private void oyunbitti_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arkaplan_Paint(object sender, PaintEventArgs e)
+        {
+
+        }        
+    }
+}
